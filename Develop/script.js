cb12bc5ceabbfb38450398c12683e1a6de7c609b
Grant Shell <grantshell33@icloud.com>
2021-04-03 21:03:53 -0500

    added readme

--- Develop/script.js
+++ Develop/script.js
@@ -4,17 +4,17 @@ var specialCharacters = `!@#$%^&*()_+`;
 var upperCaseLetters = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
 var lowerCaseLetters = `abcdefghijklmnopqrstuvwxyz`;
 var numbers = `1234567890`;
-var passwordLength;
-var confirmUpperCase;
-var confirmLowerCase;
-var confirmNumbers;
-var confirmSpecialCharacters;
-var passwordVariables;
-var userSelection;
+var passwordLength = 0;
+var confirmUpperCase = false;
+var confirmLowerCase = false;
+var confirmNumbers = false;
+var confirmSpecialCharacters = false;
+var characterBank = "";
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log("Generated Password", password);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
@@ -26,8 +26,10 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   // getting user info
-  passwordLength = prompt(
-    `How many characters will be in your new password? Please choose between 8 and 128.`
+  passwordLength = Number.parseInt(
+    prompt(
+      `How many characters will be in your new password? Please choose between 8 and 128.`
+    )
   );
   console.log("Password Length" + passwordLength);
 
@@ -37,7 +39,6 @@ function generatePassword() {
     passwordLength = prompt(`Please choose a number between 8 and 128`);
     console.log(`Password Length` + passwordLength);
   } else {
-    confirm;
     confirmUpperCase = confirm(`Do you want to include upper case letters?`);
     console.log(`Upper Case` + confirmUpperCase);
     confirmLowerCase = confirm(`Do you want to include lower case letters?`);
@@ -58,9 +59,27 @@ function generatePassword() {
     userSelection = alert(`Please choose a selection`);
   }
 
-  for (var i = 0; (i = passwordLength); i++) {
-    var userSelection =
-      passwordVariables[Math.floor(Math.random() * passwordVariables.length)];
-    console.log(userSelection);
+  //We need to add all available characters to userSelection so we can actually use them
+  if (confirmLowerCase) {
+    characterBank += lowerCaseLetters;
   }
+  if (confirmUpperCase) {
+    characterBank += upperCaseLetters;
+  }
+  if (confirmSpecialCharacters) {
+    characterBank += specialCharacters;
+  }
+  if (confirmNumbers) {
+    characterBank += numbers;
+  }
+  console.log("User Selection ->", characterBank);
+
+  var generatedPassword = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomCharacter =
+      characterBank[Math.floor(Math.random() * characterBank.length)];
+    console.log("Random Character", randomCharacter);
+    generatedPassword += randomCharacter;
+  }
+  return generatedPassword;
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 03-password-generator
+
+# Description
+
+This is a random password generator. It will allow you to create a very secure password. You have a couple variable that you need to select in order to get a password made for you.
+first you need to select how many characters you want your password. Then you get asked if you want to use upper case, lowercase, numbers and special characters. Once you have given your selections in the alerts on what you want to use you will automatically be given a randomly generated password.
